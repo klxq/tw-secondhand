5e6f038ee8713edb86ef08cf6b813ae5e9690860
Trotyl Yu <trotyl@qq.com>
2017-08-09 11:21:09 +0800

    Profile page style (#52)
    * [#50][Zejiang] fix header title
    
    * [#50][Zejiang] redirect to login when not
    
    * [#50][Zejiang] fix test
    
    * [#50][Zejiang] disable login redirect
    
    * [#50][Zejiang] add buttons

--- src/containers/pages/Profile/ProfilePage.tsx
+++ src/containers/pages/Profile/ProfilePage.tsx
@@ -1,15 +1,42 @@
 import * as React from 'react'
 import { Component } from 'react'
+import { connect, DispatchProp } from 'react-redux'
 
-import { Header, Footer } from '../../../components'
+import { RootState, UserState } from '../../../definitions'
+import { Header, Footer, Logo, Button } from '../../../components'
 
-export class ProfilePage extends Component {
+import './ProfilePage.css'
+
+export interface ProfilePageProps extends DispatchProp<void> {
+    user: UserState
+}
+
+export class ProfilePage extends Component<ProfilePageProps> {
+    constructor(props: ProfilePageProps) {
+        super(props)
+    }
+
     render() {
         return (
             <div className="App-profile">
-                <Header title="精选"/>
+                <Header title="个人信息"/>
+                <div className="logo-container">
+                    <Logo/>
+                    <span>{this.props.user.username || '未登录'}</span>
+                </div>
+                <Button content="已买宝贝"/>
+                <Button content="出售宝贝"/>
+                <Button content="退出登录"/>
                 <Footer/>
             </div>
         )
     }
 }
+
+function mapStateToProps(state: RootState): Partial<ProfilePageProps> {
+  return {
+      user: state.user,
+  }
+}
+
+export const connectedProfilePage = connect(mapStateToProps)(ProfilePage)
